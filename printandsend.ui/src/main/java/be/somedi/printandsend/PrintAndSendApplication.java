943042033f969b1f1e6ed8b7346b648a4fc58012
kenneth.vangijsel <kenneth.vangijsel@gmail.com>
2019-12-18 15:55:22 +0100

    Remove restart option. Start watchdirectory after starting the application

--- printandsend.ui/src/main/java/be/somedi/printandsend/PrintAndSendApplication.java
+++ printandsend.ui/src/main/java/be/somedi/printandsend/PrintAndSendApplication.java
@@ -1,19 +1,21 @@
 package be.somedi.printandsend;
 
+import be.somedi.printandsend.jobs.WatchServiceOfDirectory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrintAndSendApplication {
 
-    private static RestartService restartService;
+    private static WatchServiceOfDirectory watchServiceOfDirectory;
 
-    public PrintAndSendApplication(RestartService restartService) {
-        PrintAndSendApplication.restartService = restartService;
+    public PrintAndSendApplication(WatchServiceOfDirectory watchServiceOfDirectory) {
+        PrintAndSendApplication.watchServiceOfDirectory = watchServiceOfDirectory;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(PrintAndSendApplication.class, args);
-        restartService.openHomePage();
+        watchServiceOfDirectory.processEvents();
+        watchServiceOfDirectory.processEventsBeforeWatching();
     }
 }
