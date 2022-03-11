ed69c510290e5f1ec16a14bff4bb8669948fc7f5
kvangijsel <kenneth.vangijsel@somedi.be>
2022-03-11 11:16:44 +0100

    Productie properties extern plaatsen.

--- printandsend.ui/src/main/java/be/somedi/printandsend/config/PathOfProdConfig.java
+++ printandsend.ui/src/main/java/be/somedi/printandsend/config/PathOfProdConfig.java
@@ -1,11 +1,20 @@
 package be.somedi.printandsend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
-@PropertySource("path-prod.properties")
 @Profile("prod")
 public class PathOfProdConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
+        properties.setLocation(new FileSystemResource("C:/somedi/conf/conf-prod.properties"));
+        properties.setIgnoreResourceNotFound(false);
+        return properties;
+    }
 }
